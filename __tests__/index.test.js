f2c506aca612a0e76abb4e6a5f60d0d24bb2d5e1
arthur <mp3hs@ya.ru>
2023-10-24 17:34:49 +0300

    index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
+import generateDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedStylish = readFile('expectedStylish.txt');
+const expectedPlain = readFile('expectedPlain.txt');
+const expectedJson = readFile('expectedJson.txt');
+
+test.each(['json', 'yml'])('%s test', (format) => {
+  const file1 = getFixturePath(`filepath1.${format}`);
+  const file2 = getFixturePath(`filepath2.${format}`);
+  expect(generateDiff(file1, file2, 'stylish')).toEqual(expectedStylish);
+  expect(generateDiff(file1, file2, 'plain')).toEqual(expectedPlain);
+  expect(generateDiff(file1, file2, 'json')).toEqual(expectedJson);
+});
