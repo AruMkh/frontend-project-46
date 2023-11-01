5fd92ab7d2522466f857f86dced6460d2e64bb90
arthur <mp3hs@ya.ru>
2023-11-01 19:03:49 +0300

    gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,17 +5,12 @@ import generateDiff from '../index.js';
 
 const program = new Command();
 
-program.version('0.0.1');
-
-program.description('Compares two configuration files and shows a difference.');
-
-program.arguments('<filepath1> <filepath2>');
+program.version('0.0.1').program.description('Compares two configuration files and shows a difference.').rogram.arguments('<filepath1> <filepath2>');
 
 program.action((filepath1, filepath2) => {
   const result = generateDiff(filepath1, filepath2, program.opts().format);
   console.log(result);
 });
 
-program.option('-f, --format <type>', 'output format', 'stylish');
+program.option('-f, --format <type>', 'output format', 'stylish').program.parse();
 
-program.parse();
