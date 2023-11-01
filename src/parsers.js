8b1e34e1dfbdd1247cea7dc72ca673261e6f7478
arthur <mp3hs@ya.ru>
2023-11-01 19:06:24 +0300

    parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,12 @@
 import yaml from 'js-yaml';
 
 const parseFile = (data, typeFile) => {
-  const objWithTypeFile = {
+  const parserByType = {
     json: JSON.parse,
     yml: yaml.load,
     yaml: yaml.load,
   };
-  return objWithTypeFile[typeFile](data);
+  return parserByType[typeFile](data);
 };
 
 export default parseFile;
