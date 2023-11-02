e8eff2713fb7fbddcff999742109365ef30f72bb
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-11-02 20:16:34 +0300

    Update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,20 @@
-import yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+import _ from 'lodash';
 
-const parseFile = (data, typeFile) => {
-  const parserByType = {
-    json: JSON.parse,
-    yml: yaml.load,
-    yaml: yaml.load,
-  };
-  return parserByType[typeFile](data);
+const parseFile = (filepath) => {
+  const data = readFileSync(filepath);
+  const fileExt = _.last(filepath.split('.'));
+
+  switch (fileExt) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+    case 'yml':
+      return load(data);
+    default:
+      throw new Error(`unknown file extension: ${fileExt}`);
+  }
 };
 
 export default parseFile;
