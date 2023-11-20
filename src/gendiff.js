c8ef436a9db3acd852691c50595238793235b066
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-11-20 23:27:00 +0300

    Update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -24,31 +24,31 @@ const genDiff = (data1, data2) => {
     const data1hasproparty = _.has(data1, key);
     const data2hasproparty = _.has(data2, key);
 
-    if (typeof value1 === 'object' && value1 !== null && typeof value2 === 'object' && value2 != null) {
-        return {
-          keyStatus: KEY_NESTED_DIFF,
-          key,
-          children: genDiff(value1, value2),
-        };
-    }
-    if (!data2hasproparty) {
-        return { keyStatus: KEY_DELETED, key, first: value1 };
-    }
-    if (!data1hasproparty) {
-        return { keyStatus: KEY_ADDED, key, second: value2 };
-    }
-    if (value1 === value2) {
-        return {
-          keyStatus: KEY_UNCHANGED,
-          key,
-          value1: value1,
-        };
-    }
-    return {
-      keyStatus: KEY_UPDATED,
-      key,
-      value1: value1,
-      value2: value2,
+  if (typeof value1 === 'object' && value1 !== null && typeof value2 === 'object' && value2 != null) {
+      return {
+        keyStatus: KEY_NESTED_DIFF,
+        key,
+        children: genDiff(value1, value2),
+      };
+  }
+  if (!data2hasproparty) {
+      return { keyStatus: KEY_DELETED, key, first: value1 };
+  }
+  if (!data1hasproparty) {
+      return { keyStatus: KEY_ADDED, key, second: value2 };
+  }
+  if (value1 === value2) {
+      return {
+        keyStatus: KEY_UNCHANGED,
+        key,
+        value1: value1,
+      };
+  }
+  return {
+    keyStatus: KEY_UPDATED,
+    key,
+    value1: value1,
+    value2: value2,
     };
   });
 };
