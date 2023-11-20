2b143c99a638dfef0675134662eda07b52358b89
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-11-20 22:51:47 +0300

    Update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -12,7 +12,8 @@ import {
   KEY_NESTED_DIFF,
 } from './consts.js';
 
-const genDiff = (data1, data2) => {
+
+const genDiff1 = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const allKeys = [...new Set(keys1.concat(keys2))];
@@ -27,7 +28,7 @@ const genDiff = (data1, data2) => {
         return {
           keyStatus: KEY_NESTED_DIFF,
           key,
-          nestedDiff: genDiff(value1, value2),
+          nestedDiff: genDiff1(value1, value2),
         };
       }
       if (value1 === value2) {
@@ -48,6 +49,46 @@ const genDiff = (data1, data2) => {
       return { keyStatus: KEY_DELETED, key, first: value1 };
     }
     return { keyStatus: KEY_ADDED, key, second: value2 };
+  });
+};
+
+const genDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const allKeys = [...new Set(keys1.concat(keys2))];
+  const sortedKeys = _.sortBy(allKeys);
+
+  return sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    const data1hasproparty = _.has(data1, key);
+    const data2hasproparty = _.has(data2, key);
+
+    if (typeof value1 === 'object' && value1 !== null && typeof value2 === 'object' && value2 != null) {
+        return {
+          keyStatus: KEY_NESTED_DIFF,
+          key,
+          children: genDiff(value1, value2),
+        };
+    if (!data2hasproparty) {
+        return { keyStatus: KEY_DELETED, key, first: value1 };
+    }
+    if (!data1hasproparty) {
+        return { keyStatus: KEY_ADDED, key, second: value2 };
+    }
+    if (value1 === value2) {
+        return {
+          keyStatus: KEY_UNCHANGED,
+          key,
+          value1: value1,
+        };
+    }
+    return {
+      keyStatus: KEY_UPDATED,
+      key,
+      value1: value1,
+      value2: value2,
+    };
   });
 };
 
