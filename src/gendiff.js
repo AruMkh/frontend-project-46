aa1ecce10010e36e10da2442f2364ed937f718e4
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-11-20 23:15:19 +0300

    Update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -12,46 +12,6 @@ import {
   KEY_NESTED_DIFF,
 } from './consts.js';
 
-
-const genDiff1 = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const allKeys = [...new Set(keys1.concat(keys2))];
-  const sortedKeys = _.sortBy(allKeys);
-
-  return sortedKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (Object.prototype.hasOwnProperty.call(data1, key)
-        && Object.prototype.hasOwnProperty.call(data2, key)) {
-      if (typeof value1 === 'object' && value1 !== null && typeof value2 === 'object' && value2 != null) {
-        return {
-          keyStatus: KEY_NESTED_DIFF,
-          key,
-          nestedDiff: genDiff1(value1, value2),
-        };
-      }
-      if (value1 === value2) {
-        return {
-          keyStatus: KEY_UNCHANGED,
-          key,
-          first: value1,
-        };
-      }
-      return {
-        keyStatus: KEY_UPDATED,
-        key,
-        first: value1,
-        second: value2,
-      };
-    }
-    if (Object.prototype.hasOwnProperty.call(data1, key)) {
-      return { keyStatus: KEY_DELETED, key, first: value1 };
-    }
-    return { keyStatus: KEY_ADDED, key, second: value2 };
-  });
-};
-
 const genDiff = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
