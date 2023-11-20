35ec9706c3155d8aafd00ad69f971f2f85e379c1
arthur <mp3hs@ya.ru>
2023-11-21 02:20:51 +0300

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,7 +18,7 @@ const formatValue = (val, nestingLevel) => {
     const bracketIdents = ' '.repeat(nestingLevel * spaceMultiplier);
     return `{\n${result}${bracketIdents}}`;
   }
-  return val?.toString();
+  return val ? val.toString() : null;
 };
 
 const formatObjectDiff = (objectDiff, nestingLevel) => {
