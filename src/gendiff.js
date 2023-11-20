679045b113afe7cdfe8716ece6d122d6e9cff4a8
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-11-20 21:21:37 +0300

    Update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -54,8 +54,14 @@ const parseFilesAndGenDiff = (filepath1, filepath2, format = 'stylish') => {
   const absolutePath1 = resolve(filepath1);
   const absolutePath2 = resolve(filepath2);
 
-  const data1 = parseFile(absolutePath1);
-  const data2 = parseFile(absolutePath2);
+  const fileData1 = readFileSync(absolutePath1);
+  const fileExt1 = _.last(absolutePath1.split('.'));
+
+  const fileData2 = readFileSync(absolutePath2);
+  const fileExt2 = _.last(absolutePath2.split('.'));
+  
+  const data1 = parseFile(fileData1, fileExt1);
+  const data2 = parseFile(fileData2, fileExt2);
   const result = genDiff(data1, data2);
 
   const formatter = getFormatter(format);
