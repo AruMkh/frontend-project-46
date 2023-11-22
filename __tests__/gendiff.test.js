3d90674f85c8953280d56274b9dc43c5478ab0fc
arthur <mp3hs@ya.ru>
2023-11-22 22:28:40 +0300

    fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { readFileSync } from 'fs';
 
-import fileGendiff from '../src/index.js';
+import fileGendiff from '../src/index';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
